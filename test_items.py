440b3ad6c687dbdbcc5aa8fbdc8960dbf686416d
Memet Dzhamalov <memet.dzhamalov@exinity.com>
2024-02-20 17:12:44 +0400

    added time.sleep to check "Fr" language

--- test_items.py
+++ test_items.py
@@ -1,8 +1,11 @@
+import time
+
 from selenium.webdriver.common.by import By
 
 
 def test_is_button_add_to_basket_present(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
+    time.sleep(5)
     add_to_basket = len(browser.find_elements(By.XPATH, "//form[@id='add_to_basket_form']"))
     assert add_to_basket > 0, "Can't find button 'Add to basket'"
